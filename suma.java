8233695fb0933b42220e9c024882d57ddaefb6f5
nabil acharki <nabilacharki89@gmail>
2018-09-19 18:36:36 +0200

    He creat un nou arxiu anomenat suma.java
    idqhinqeobncdo

---
+++ suma.java
@@ -0,0 +1,11 @@
+
+int numero1= 0;
+int numero2= 0;
+
+System.out.printIN("Introdueix el primer numero:");
+numero1 = reader.nextInt();
+
+System.out.printIN("Introdueix el segon numero:");
+numero2 = reader.nextInt();
+
+
