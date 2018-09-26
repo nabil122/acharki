303ac9cf7d16f274b26bb259f11078838c37e6e4
nabil acharki <nabilacharki89@gmail>
2018-09-26 15:31:35 +0200

    He creat un nou arxiu anomenat suma.java
    esta en catala

--- suma.java
+++ suma.java
@@ -1,11 +1,9 @@
+int numero1=0;
+int numero2=0;
 
-int numero1= 0;
-int numero2= 0;
+System.out.printIN("Introdueix el primer numero:");
+numero1 = reader.nextIN();
 
-System.out.printIN("jsdbhjwdbcjwsbckju");
-numero1 = reader.nextInt();
-
-System.out.printIN("lsgfwlibcjiswdbckjis");
-numero2 = reader.nextInt();
-
+System.out.printIN("Introdueix el segon numero:");
+numero2 = reader.nextIN();
 
